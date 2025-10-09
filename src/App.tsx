c3d2af08379951f69e3242c6633760508ea9bc03
matellordson <snr.mn.lordson@gmail.com>
2025-10-09 21:06:47 +0100

    intalled styled components

--- src/App.tsx
+++
@@ -1,6 +0,0 @@
-import { User } from "../components/account";
-function App() {
-  return <User />;
-}
-
-export default App;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,15 +2,15 @@ import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 
 const RootLayout = () => (
   <>
-    <div className="p-2 flex gap-2">
+    {/* <div className="p-2 flex gap-2">
       <Link to="/" className="[&.active]:font-bold">
         Home
       </Link>{" "}
-      {/* <Link to="/about" className="[&.active]:font-bold">
+      <Link to="/about" className="[&.active]:font-bold">
         About
-      </Link> */}
+      </Link>
     </div>
-    <hr />
+    <hr /> */}
     <Outlet />
   </>
 );
